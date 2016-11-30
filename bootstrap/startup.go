d61738713c368540caaf30123f38fa6d6746e8e8
Anish Goyal <anishgoyal@outlook.com>
2016-11-30 18:09:27 -0500

    Add a query for getting count of explanations

--- bootstrap/startup.go
+++ bootstrap/startup.go
@@ -9,6 +9,7 @@ import (
 
 	"abraham_linkedin/constants"
 	"abraham_linkedin/controllers"
+	"abraham_linkedin/models"
 )
 
 // GlobalStart begins initialization for the application,
@@ -32,6 +33,16 @@ func GlobalStart() bool {
 
 	fmt.Println("[STARTUP] Creating Routes")
 	CreateRoutes()
+
+	counts, err := models.GetExplanationCounts(db)
+	if err == nil {
+		for _, count := range counts {
+			if count.Count < 1000 {
+				continue
+			}
+			fmt.Println("Explanation \""+count.Explanation+"\":", count.Count)
+		}
+	}
 
 	fmt.Println("[STARTUP] Starting server on port " +
 		strconv.Itoa(constants.PortNum))
