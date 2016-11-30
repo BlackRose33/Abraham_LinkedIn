f16729abdd25fc82fb5d265a3f4bd5d9b1273a7e
Anish Goyal <anishgoyal@outlook.com>
2016-11-30 18:02:32 -0500

    Add get contributions for each year

--- models/candidate.go
+++ models/candidate.go
@@ -53,12 +53,12 @@ func GetCandidates(db *sql.DB) ([]Candidate, error) {
 
 // CandidateAmount holds information about expenditures from a candidate
 type CandidateAmount struct {
-	ID        string
-	FirstName string
-	LastName  string
-	Year      int
-	Amount    float64
-	ExpName   string
+	ID         string
+	FirstName  string
+	LastName   string
+	Year       int
+	Amount     float64
+	EntityName string
 }
 
 // GetBiggestSpenderOfAnyYear gets the... biggest spender of any year?
@@ -76,11 +76,38 @@ func GetBiggestSpenderOfAnyYear(db *sql.DB) (*CandidateAmount, error) {
 
 	if rows.Next() {
 		var candidate CandidateAmount
-		err = rows.Scan(&candidate.ExpName, &candidate.Amount, &candidate.Year,
+		err = rows.Scan(&candidate.EntityName, &candidate.Amount, &candidate.Year,
 			&candidate.ID, &candidate.FirstName, &candidate.LastName)
 		if err == nil {
 			return &candidate, nil
 		}
 	}
 	return nil, nil
+}
+
+// GetCandidateContributionsForEachYear attempts to find the amount of
+// total contributions a candidate received in any year
+func GetCandidateContributionsForEachYear(db *sql.DB, candID string) (
+	[]CandidateAmount, error) {
+
+	rows, err := db.Query("SELECT c1.con_name, c1.con_amount, c1.election_year, "+
+		"c1.cand_id, c2.cand_first, c2.cand_last FROM contributes c1 JOIN "+
+		"candidate c2 ON c1.cand_id = c2.cand_id WHERE c1.cand_id = ? "+
+		"ORDER BY con_amount DESC", candID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	amounts := make([]CandidateAmount, 0, 5)
+	for rows.Next() {
+		var amount CandidateAmount
+		err = rows.Scan(&amount.EntityName, &amount.Amount, &amount.Year,
+			&amount.ID, &amount.FirstName, &amount.LastName)
+		if err == nil {
+			amounts = append(amounts, amount)
+		}
+	}
+
+	return amounts, nil
 }
