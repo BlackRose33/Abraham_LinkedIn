0a27de0f78b70237d831a53f5dcdbb3bf1984412
blw68 <brianl.wong@rutgers.edu>
2016-12-01 14:20:33 -0500

    updated expenditure.go and contribution.go

--- models/contribution.go
+++ models/contribution.go
@@ -1,9 +1,35 @@
 package models
 
-// Contribution bblallasldfkahwperoiajnpwercqunpr98unio
+// Contribution struct with year, amount, first name, last name, occupation
 type Contribution struct {
 	Year      int
 	Amount    float64
 	FirstName string
 	LastName  string
+	Occupation string
+}
+
+// CountOccupation counts the number of each occupation
+func CountOccupation(db *sql.DB) (*Contribution, error) {
+	rows, err := db.Query("select occupation, count(occupation) " +
+		"from contributes " +
+		"group by occupation " +
+		"order by count(occupation) desc")
+
+  if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+  amounts := make([]Contribution, 0, 25)
+	for rows.Next() {
+		var amount Contribution
+		err = rows.Scan(&amount.Occupation, &amount.Count)
+		if err == nil {
+			amounts = append(amounts, amount)
+		}
+	}
+
+	return amounts, nil
 }
--- models/expenditure.go
+++ models/expenditure.go
@@ -1,5 +1,59 @@
 package models
 
-// Expenditure qv23ni5vqptujpwtunqpb
+// Expenditure struct with sched, c_code
 type Expenditure struct {
+  Sched string
+  C_code string
+  Count int
+}
+
+// CountSchedules counts the number of schedules
+func CountSchedules(db *sql.DB) (*Expenditure, error) {
+  rows, err := db.Query("select sched, count(sched) " +
+    "from expenditures " +
+    "group by sched " +
+    "order by count(sched) desc")
+
+  if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+  amounts := make([]Expenditure, 0, 6)
+	for rows.Next() {
+		var amount Expenditure
+		err = rows.Scan(&amount.Sched, &amount.Count)
+		if err == nil {
+			amounts = append(amounts, amount)
+		}
+	}
+
+	return amounts, nil
+
+}
+
+// CountCCodes counts the number of c_codes
+func CountCCodes(db *sql.DB) (*Expenditure, error) {
+  rows, err := db.Query("select c_code, count(c_code) " +
+    "from expenditures " +
+    "group by c_code " +
+    "order by count(c_code) desc")
+
+  if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+  amounts := make([]Expenditure, 0, 6)
+	for rows.Next() {
+		var amount Expenditure
+		err = rows.Scan(&amount.C_code, &amount.Count)
+		if err == nil {
+			amounts = append(amounts, amount)
+		}
+	}
+
+	return amounts, nil
 }
